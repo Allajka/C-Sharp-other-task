c0b7ccd79018747b747b7ca323cab110d26b4720
allajka <allajka94@gmail.com>
2022-08-11 11:54:52 +0300

    example014-example015

--- intern/example013/Program.cs
+++ intern/example013/Program.cs
@@ -1,14 +1,21 @@
 ﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
 
-double userNumberA = 28;
-double userNumberB = 3;
+double userNumberA = 1;
+double userNumberB = 0;
 double result = userNumberA % userNumberB;
 
-if (result == userNumberA)
+if (userNumberA == 0 || userNumberB == 0) 
+{
+    Console.Write("Число 0 кратно любому числу в 0 раз."); 
+}
+else 
+{
+if (result == 0 || result == userNumberA)
 {
     Console.Write($"Число {userNumberA} кратно {userNumberB}.");
 }
 else
 {
     Console.Write($"Число {userNumberA} не кратно {userNumberB}. Остаток от деления {result}.");
+}
 }
---
+++ junior/example015/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());;
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно одновременно 7 и 23.");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ кратно одновременно 7 и 23.");
+}
