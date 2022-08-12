ab8c989577a14cfdbf7ad067fc85911780ac04dc
allajka <allajka94@gmail.com>
2022-08-12 17:03:31 +0300

    example 023/024/018

---
+++ middle/example023/Program.cs
@@ -0,0 +1,17 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());;
+int index = 1;
+int square = 0;
+
+while (index <= N)
+{
+    square = index * index;
+    Console.Write(" " + square);
+    index++;
+    if (index % 10 == 0 )
+    {
+        Console.WriteLine();
+    }
+}
---
+++ middle/example024/Program.cs
@@ -0,0 +1,17 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());;
+int index = 1;
+int cube = 0;
+
+while (index <= N)
+{
+    cube = index * index* index;
+    Console.Write(" " + cube);
+    index++;
+    if (index % 10 == 0 )
+    {
+        Console.WriteLine();
+    }
+}
