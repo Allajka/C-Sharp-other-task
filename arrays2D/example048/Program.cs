1473a6e4c72ddc44763eb5f35852365a8ea9082b
allajka <allajka94@gmail.com>
2022-08-23 11:13:06 +0300

    example048-049

---
+++ arrays2D/example048/Program.cs
@@ -0,0 +1,28 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+void PrintArray (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j], 3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] CreateArray (int row, int col)
+{
+    int[,] numbers = new int [row,col];
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers [i,j] = new Random().Next(0, 100);
+        }
+    }
+    return numbers;
+}
+int[,] numbers = CreateArray(row: 5, col: 4);
+PrintArray(numbers);
---
+++ arrays2D/example049/Program.cs
@@ -0,0 +1,28 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+
+void PrintArray (double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j], 8}");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] CreateArray (int row, int col)
+{
+    double[,] numbers = new double [row,col];
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers [i,j] = Math.Round(new Random().NextDouble() *100, 2);
+        }
+    }
+    return numbers;
+}
+double[,] numbers = CreateArray(row: 5, col: 4);
+PrintArray(numbers);
