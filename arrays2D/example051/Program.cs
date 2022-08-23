8422255965f4267abe9ea99e98122244f6946140
allajka <allajka94@gmail.com>
2022-08-23 16:21:45 +0300

    example051-052

---
+++ arrays2D/example051/Program.cs
@@ -0,0 +1,31 @@
+﻿//51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] CreateArray(int row, int col)
+{
+    int[,] number = new int[row, col];
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+        for (int j = 0; j < number.GetLength(1); j++)
+        {
+            number[i, j] = i + j;
+        }
+    }
+    return number;
+}
+
+int [,] numbers = CreateArray(4, 4);
+PrintArray(numbers);
+System.Console.WriteLine();
+PrintArray(numbers);
---
+++ arrays2D/example052/Program.cs
@@ -0,0 +1,46 @@
+﻿// 52. В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j],3} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] CreateArray(int row, int col)
+{
+    int[,] number = new int[row, col];
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+        for (int j = 0; j < number.GetLength(1); j++)
+        {
+            number[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return number;
+}
+
+int[,] ChangeEvenPositions(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+            {
+                arr[i, j] = arr[i, j] * arr[i, j];
+            }
+        }
+    }
+    return arr;
+}
+int[,] numbers = CreateArray(5, 5);
+PrintArray(numbers);
+System.Console.WriteLine();
+ChangeEvenPositions(numbers);
+PrintArray(numbers);
